277b137b43ecd14e29361cd305c75e2666fd19b9
raghadsam <raghad66samrout@gmail.com>
2021-09-01 22:10:28 +0300

    approved

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from "react";
 import "./App.css";
+import objectDetectionSketch from "./ObjectDetectionSketch";
+
+import P5Wrapper from "react-p5-wrapper";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+  return <P5Wrapper sketch={objectDetectionSketch} />;
 }
 
 export default App;
